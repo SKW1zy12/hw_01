2e6fb1aaac9c75f8c1a0de714357dfd0f0920ebd
SKW1zy12 <lohmuhamatasiz@gmail.com>
2023-10-08 19:04:30 +0600

    hw_02

--- lesson_01.py
+++ lesson_01.py
@@ -23,32 +23,32 @@
 # user.info_user()
 
 
-class Car:
-    def __init__(self, brand, model, year,type_fuel):
-        self.brand = brand
-        self.model = model
-        self.year = year
-        self.type_fuel = type_fuel
+# class Car:
+#     def __init__(self, brand, model, year,type_fuel):
+#         self.brand = brand
+#         self.model = model
+#         self.year = year
+#         self.type_fuel = type_fuel
 
-    def info(self):
-        print(f"Марка: {self.brand}, Модель: {self.model}, Год: {self.year}, Топливо: {self.type_fuel}")
+#     def info(self):
+#         print(f"Марка: {self.brand}, Модель: {self.model}, Год: {self.year}, Топливо: {self.type_fuel}")
     
-    def cars(self):
-        print("Машина завелась")
-        n = float(0)
-        while True:
-            n += 0.5
-            if n == 20:
-                print("Вы проехали 20км")
-            elif n == 50:
-                print("Вы проехали 50 км")
-            elif n == 80:
-                print("Вы проехали 80км")
-                print("У вас мало топливо")
-            elif n == 100:
-                print("Вы проехали 100км")
-                print("Машина остоновлена< не осталось топливо")
-                break
-car = Car("Tayota", "Camry 75", "2022", "Бензин") 
-car.info()    
-car.cars()       
+#     def cars(self):
+#         print("Машина завелась")
+#         n = float(0)
+#         while True:
+#             n += 0.5
+#             if n == 20:
+#                 print("Вы проехали 20км")
+#             elif n == 50:
+#                 print("Вы проехали 50 км")
+#             elif n == 80:
+#                 print("Вы проехали 80км")
+#                 print("У вас мало топливо")
+#             elif n == 100:
+#                 print("Вы проехали 100км")
+#                 print("Машина остоновлена< не осталось топливо")
+#                 break
+# car = Car("Tayota", "Camry 75", "2022", "Бензин") 
+# car.info()    
+# car.cars()       
